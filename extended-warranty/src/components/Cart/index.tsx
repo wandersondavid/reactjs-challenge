627e705c3d261b3899b9c6b2fb1abfd38a71c80e
wandersondavid <wandersondavid.ifg@gmail.com>
2023-04-22 21:25:01 -0300

    refactor: ajuste code

--- extended-warranty/src/components/Cart/index.tsx
+++ extended-warranty/src/components/Cart/index.tsx
@@ -3,14 +3,14 @@ import { Box, Card, CardContent, IconButton, Typography } from "@mui/material"
 import { ShoppingBasket } from "@mui/icons-material"
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { Link } from "react-router-dom"
-import { Cart as CartProps} from "../../types/produts"
+import { Cart as CartProps } from "../../types/produts"
 import { formatMoney } from "../../utils/money"
 
 type Props = {
   cart: CartProps;
 }
 
-export const Cart = ({cart}: Props ) => {
+export const Cart = ({ cart }: Props) => {
 
   return (
     <Card sx={{
@@ -22,22 +22,29 @@ export const Cart = ({cart}: Props ) => {
       alignItems: 'center',
     }}>
 
-      <CardContent sx={{ width: `200px`, display: 'flex', alignItems: 'center', backgroundColor: '#D2FEE1', borderRadius:'8px' }}>
+      <CardContent sx={{ width: `200px`, display: 'flex', alignItems: 'center', backgroundColor: '#D2FEE1', borderRadius: '8px' }}>
         <IconButton >
-          <ShoppingBasket sx={{color: '#055F5B'}} />
+          <ShoppingBasket sx={{ color: '#055F5B' }} />
         </IconButton>
 
-        <Typography component="div" variant="h5" sx={{ fontSize: '1.25rem', textAlign: 'center', width:'100%', fontWeight: 700 }}>
-          {!!Object.keys(cart).length && formatMoney(Object.entries(cart).reduce((acc, [_, value]) => acc + value.amount * value.price, 0), 'BRL')}
+        <Typography component="div" variant="h5" sx={{ fontSize: '1.25rem', textAlign: 'center', width: '100%', fontWeight: 700 }}>
+
+          {!!Object.keys(cart).length &&
+            formatMoney(Object.entries(cart).reduce((acc, [_, value]) => acc + value.amount * value.price, 0), 'BRL')
+          }
+
           {!Object.keys(cart).length && formatMoney(0, 'BRL')}
+
         </Typography>
       </CardContent>
-      <Box sx={{display: 'flex', alignItems: 'center', fontSize: '1rem' }}>
-        <Link to={'/cart'} style={{color:'blue'}} >To Cart</Link>
-        <IconButton >
-          <ArrowForwardIosIcon sx={{color: '#055F5B'}} />
-        </IconButton>
-      </Box>
+
+      {!!Object.keys(cart).length &&
+        <Box sx={{ display: 'flex', alignItems: 'center', fontSize: '1rem' }}>
+          <Link to={'/cart'} style={{ color: 'blue' }} >Finalizar compra</Link>
+          <IconButton >
+            <ArrowForwardIosIcon sx={{ color: '#055F5B' }} />
+          </IconButton>
+        </Box>}
 
     </Card>
   )
