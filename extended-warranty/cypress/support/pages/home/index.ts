baba70670b42a8b0d2f4d3f342473bdc39dc3d41
wandersondavid <wandersondavid.ifg@gmail.com>
2023-05-02 18:04:40 -0300

    style: comment code

--- extended-warranty/cypress/support/pages/home/index.ts
+++ extended-warranty/cypress/support/pages/home/index.ts
@@ -1,33 +1,33 @@
 export const go = (url: string) =>
   cy.visit(url);
 
-export const click = (selector: string) =>
-  cy.get(selector).click();
+// export const click = (selector: string) =>
+//   cy.get(selector).click();
 
-export const clickLink = (selector: string) =>
-  cy.get(`a[href="${selector}"]`).click();
+// export const clickLink = (selector: string) =>
+//   cy.get(`a[href="${selector}"]`).click();
 
-export const clickButton = (selector: string) =>
-  cy.get(`button:contains("${selector}")`).click();
+// export const clickButton = (selector: string) =>
+//   cy.get(`button:contains("${selector}")`).click();
 
-export const clickButtonWithText = (selector: string) =>
-  cy.get(`button:contains("${selector}")`).click();
+// export const clickButtonWithText = (selector: string) =>
+//   cy.get(`button:contains("${selector}")`).click();
 
-export const clickButtonWithTextAndSelector =
-  (selector: string, text: string) =>
-    cy.get(`${selector}:contains("${text}")`).click();
+// export const clickButtonWithTextAndSelector =
+//   (selector: string, text: string) =>
+//     cy.get(`${selector}:contains("${text}")`).click();
 
-export const clickButtonWithTextAndSelectorAndIndex =
-  (selector: string, text: string, index: number) =>
-    cy.get(`${selector}:contains("${text}")`).eq(index).click();
+// export const clickButtonWithTextAndSelectorAndIndex =
+//   (selector: string, text: string, index: number) =>
+//     cy.get(`${selector}:contains("${text}")`).eq(index).click();
 
-export const clickButtonWithTextAndIndex = (selector: string, index: number) =>
-  cy.get(`button:contains("${selector}")`).eq(index).click();
+// export const clickButtonWithTextAndIndex = (selector: string, index: number) =>
+//   cy.get(`button:contains("${selector}")`).eq(index).click();
 
-export const clickButtonWithTextAndSelectorAndIndexAndSelector =
-  (selector: string, text: string, index: number, selector2: string) =>
-    cy.get(`${selector}:contains("${text}")`).eq(index).get(selector2).click();
+// export const clickButtonWithTextAndSelectorAndIndexAndSelector =
+//   (selector: string, text: string, index: number, selector2: string) =>
+//     cy.get(`${selector}:contains("${text}")`).eq(index).get(selector2).click();
 
-export const clickButtonWithTextAndSelectorAndIndexAndSelectorAndIndex =
-  (selector: string, text: string, index: number, selector2: string, index2: number) =>
-    cy.get(`${selector}:contains("${text}")`).eq(index).get(selector2).eq(index2).click();
+// export const clickButtonWithTextAndSelectorAndIndexAndSelectorAndIndex =
+//   (selector: string, text: string, index: number, selector2: string, index2: number) =>
+//     cy.get(`${selector}:contains("${text}")`).eq(index).get(selector2).eq(index2).click();
