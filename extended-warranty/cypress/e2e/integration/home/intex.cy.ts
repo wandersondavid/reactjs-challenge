40414199b64f720990f310c3d37434faf8a5457c
wandersondavid <wandersondavid.ifg@gmail.com>
2023-05-02 17:16:46 -0300

    feat: add util to test

--- extended-warranty/cypress/e2e/integration/home/intex.cy.ts
+++ extended-warranty/cypress/e2e/integration/home/intex.cy.ts
@@ -1,32 +1,34 @@
+import { go } from "../../../support/pages/home";
+
 describe("Sales", () => {
 
   it("Visit the sales page", () => {
-    cy.visit("http://localhost:5173/");
+    go("http://localhost:5173/");
     cy.get('span').contains("R$ 0,00");
   });
 
   it("Add product to cart", () => {
-    cy.visit("http://localhost:5173/");
+    go("http://localhost:5173/");
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra");
   });
 
   it("Add product to cart and go to cart", () => {
-    cy.visit("http://localhost:5173/");
+    go("http://localhost:5173/");
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').click();
     cy.get("button").contains("Finalizar Compra");
   });
 
   it("Add product to cart and remove product to cart", () => {
-    cy.visit("http://localhost:5173/");
+    go("http://localhost:5173/");
     cy.get("button").contains("Comprar").click();
     cy.get("button").contains("-").click();
     cy.get('a[href="/cart"]').should("not.exist");
   });
 
   it("Remove product to cart and add product to cart", () => {
-    cy.visit("http://localhost:5173/");
+    go("http://localhost:5173/");
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').click();
     cy.get("button").contains("-").click();
@@ -34,7 +36,7 @@ describe("Sales", () => {
   });
 
   it("Add product to cart and finish sales", () => {
-    cy.visit("http://localhost:5173/");
+    go("http://localhost:5173/");
     cy.get("button").contains("Comprar").click();
     cy.get("button").contains("+").click();
     cy.get('a[href="/cart"]').click();
