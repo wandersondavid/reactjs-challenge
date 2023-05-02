63a937a4ba54cc4acf8a65e60385afd1a1665165
wandersondavid <wandersondavid.ifg@gmail.com>
2023-05-02 17:09:03 -0300

    refactor: ajustes code  add test product to cart and finish sales

--- extended-warranty/cypress/e2e/integration/home/intex.cy.ts
+++ extended-warranty/cypress/e2e/integration/home/intex.cy.ts
@@ -1,19 +1,14 @@
 describe("Sales", () => {
+
   it("Visit the sales page", () => {
     cy.visit("http://localhost:5173/");
+    cy.get('span').contains("R$ 0,00");
   });
 
   it("Add product to cart", () => {
     cy.visit("http://localhost:5173/");
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra");
-  });
-
-  it("Remove product to cart", () => {
-    cy.visit("http://localhost:5173/");
-    cy.get("button").contains("Comprar").click();
-    cy.get("button").contains("-").click();
-    cy.get('a[href="/cart"]').should("not.exist");
   });
 
   it("Add product to cart and go to cart", () => {
@@ -37,4 +32,13 @@ describe("Sales", () => {
     cy.get("button").contains("-").click();
     cy.get('button').contains("Conferir produtos").click();
   });
+
+  it("Add product to cart and finish sales", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("button").contains("Comprar").click();
+    cy.get("button").contains("+").click();
+    cy.get('a[href="/cart"]').click();
+    cy.get("button").contains("Finalizar Compra").click();
+  });
+
 });
