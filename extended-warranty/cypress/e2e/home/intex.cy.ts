c7a760d6bbffb38176151f4297c931d6f054498f
wandersondavid <wandersondavid.ifg@gmail.com>
2023-05-02 15:25:56 -0300

    refactor: test e2e page home

--- extended-warranty/cypress/e2e/home/intex.cy.ts
+++ extended-warranty/cypress/e2e/home/intex.cy.ts
@@ -5,35 +5,36 @@ describe("Sales", () => {
 
   it("Add product to cart", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("button").contains("+").click();
+    cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra");
   });
 
   it("Remove product to cart", () => {
     cy.visit("http://localhost:5173/");
+    cy.get("button").contains("Comprar").click();
     cy.get("button").contains("-").click();
     cy.get('a[href="/cart"]').should("not.exist");
   });
 
   it("Add product to cart and go to cart", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("button").contains("+").click();
+    cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').click();
     cy.get("button").contains("Finalizar Compra");
   });
 
   it("Add product to cart and remove product to cart", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("button").contains("+").click();
+    cy.get("button").contains("Comprar").click();
     cy.get("button").contains("-").click();
     cy.get('a[href="/cart"]').should("not.exist");
   });
 
   it("Remove product to cart and add product to cart", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("button").contains("+").click();
+    cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').click();
     cy.get("button").contains("-").click();
-    cy.get('a[href="/"]').click();
+    cy.get('button').contains("Conferir produtos").click();
   });
 });
