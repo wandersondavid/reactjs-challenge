e1e155da3f1ab8969d42918d87fefd2994f0f260
wandersondavid <wandersondavid.ifg@gmail.com>
2023-05-03 17:58:49 -0300

    refactor: add new tests page cart

--- extended-warranty/cypress/e2e/integration/cart/index.cy.ts
+++ extended-warranty/cypress/e2e/integration/cart/index.cy.ts
@@ -3,24 +3,25 @@ import { go } from "../../../support/pages/home";
 describe("Cart", () => {
   const BASE_URL = "http://localhost:5173/";
 
+
+  beforeEach(()=>(
+    go(BASE_URL)
+  ))
+
   it("Add product to cart", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra").click();
     cy.get("button").contains("+").click();
     cy.get('span').contains("2");
-
     cy.get('p').contains("Seus Produtos");
     cy.get('p').contains("Resumo");
   });
 
   it("Verify has extended warranty", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra").click();
     cy.get("button").contains("+").click();
     cy.get('span').contains("2");
-
     cy.get('h3').contains("Garantia Estendida");
     cy.get('p').contains("Voce pode estender o tempo de garantia deste produto");
     cy.get('span').contains("Sem garantia");
@@ -29,15 +30,12 @@ describe("Cart", () => {
 
   it("Verify has extended warranty and add 1 year", () => {
 
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra").click();
     cy.get("button").contains("+").click();
     cy.get('span').contains("2");
-
     cy.get('h3').contains("Garantia Estendida");
     cy.get('p').contains("Voce pode estender o tempo de garantia deste produto");
-
     cy.get('span[data-index="1"]').contains("1 ano").click();
     cy.get('span').contains("1 ano");
 
@@ -45,7 +43,6 @@ describe("Cart", () => {
 
   it("Verify has extended warranty and add 2 year", () => {
 
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra").click();
     cy.get("button").contains("+").click();
@@ -60,19 +57,36 @@ describe("Cart", () => {
   })
 
   it("Verify has extended warranty and add 3 year", () => {
-
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get('a[href="/cart"]').contains("Finalizar compra").click();
     cy.get("button").contains("+").click();
     cy.get('span').contains("2");
-
     cy.get('h3').contains("Garantia Estendida");
     cy.get('p').contains("Voce pode estender o tempo de garantia deste produto");
-
     cy.get('span[data-index="3"]').contains("3 ano").click();
     cy.get('span').contains("3 ano");
-
   })
+
+  it("When reloading the page, the cart should be retained", () => {
+    cy.get("button").contains("Comprar").click();
+    cy.get('a[href="/cart"]').contains("Finalizar compra").click();
+    cy.get("button").contains("+").click();
+    cy.wait(1000);
+    cy.reload();
+    cy.get("p").contains("Seus Produtos").should("exist");
+    cy.get("p").contains("Resumo").should("exist");
+    cy.get("span").contains('2').should("exist");
+  });
+
+  it("To page buying", () => {
+    cy.get("button").contains("Comprar").click();
+    cy.get('a[href="/cart"]').contains("Finalizar compra").click();
+    cy.get("button").contains("+").click();
+    cy.get("p").contains("Seus Produtos").should("exist");
+    cy.get("p").contains("Resumo").should("exist");
+    cy.get("span").contains('2').should("exist");
+    cy.get("button").contains("Continuar").click();
+    cy.get("h2").contains("Seus Produtos").should("exist");
+  });
 
 })
