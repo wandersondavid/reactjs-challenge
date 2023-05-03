70b1f1143bce3cb715ff347e0a28c26a7be9ffa6
wandersondavid <wandersondavid.ifg@gmail.com>
2023-05-03 17:57:32 -0300

    refactor: test page home

--- extended-warranty/cypress/e2e/integration/home/intex.cy.ts
+++ extended-warranty/cypress/e2e/integration/home/intex.cy.ts
@@ -5,49 +5,39 @@ describe("Sales", () => {
   const BASE_URL = "http://localhost:5173/";
   const CART_LINK_SELECTOR = 'a[href="/cart"]';
 
+  beforeEach(()=>(
+    go(BASE_URL)
+  ))
+
   it("Visit the sales page", () => {
-    go(BASE_URL);
     cy.get('span').contains("R$ 0,00");
   });
 
   it("Add product to cart", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get(CART_LINK_SELECTOR).contains("Finalizar compra");
   });
 
   it("Add product to cart and remove product to cart", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get("button").contains("-").click();
     cy.get(CART_LINK_SELECTOR).should("not.exist");
   });
 
   it("Remove product to cart and add product to cart", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get(CART_LINK_SELECTOR).click();
     cy.get("button").contains("-").click();
     cy.get('button').contains("Conferir produtos").click();
   });
 
-  // it("Add product to cart and finish sales", () => {
-  //   go(BASE_URL);
-  //   cy.get("button").contains("Comprar").click();
-  //   cy.get("button").contains("+").click();
-  //   cy.get(CART_LINK_SELECTOR).click();
-  //   cy.get("button").contains("Finalizar Compra").click();
-  // });
-
   it("add product to cart verify if the product is in the cart", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get(CART_LINK_SELECTOR).click();
     cy.get("p").contains("Seus Produtos").should("exist");
   });
 
   it("add product to cart and remove product to cart verify if the product is not in the cart", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.get(CART_LINK_SELECTOR).click();
     cy.get("button").contains("-").click();
@@ -56,11 +46,10 @@ describe("Sales", () => {
   });
 
   it("When reloading the page, the cart should be retained", () => {
-    go(BASE_URL);
     cy.get("button").contains("Comprar").click();
     cy.wait(1000);
     cy.reload();
-    cy.get("p").contains("Carregando...").should("exist");
+    cy.get("p").should("exist").contains("Carregando...");
     cy.get("h6").contains('1').should("exist");
     cy.get("span").should('not.equal', 'R$ 0,00');
   });
