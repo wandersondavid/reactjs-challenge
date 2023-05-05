8a276add21bd7d4577e258af90f9ea6250b27948
wandersondavid <wandersondavid.ifg@gmail.com>
2023-05-05 00:12:12 -0300

    feat: type  obj body and add tratamento de erros

--- server/src/Routes/Checkout.ts
+++ server/src/Routes/Checkout.ts
@@ -6,11 +6,21 @@ const FRONT_DOMAIN = process.env.FRONT_DOMAIN
 
 const stripe = require('stripe')(STRIPE_SECRET_KEY);
 
+type product = {
+  price: string,
+  quantity: number
+}
+
+type body = {
+  line_items: product[],
+}
 
 export async function checkoutRoutes(fastify: FastifyInstance) {
   fastify.post('/create-checkout-session', async (request, reply) => {
 
-    const body = request.body
+    const body = request.body as body
+
+    if(!body.line_items.length) throw new Error('No line items provided')
 
     try {
 
